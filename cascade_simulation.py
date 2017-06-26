f41cfe2f8b9a287f92b7a5362c9a318c4be0915a
Henning Schiebenhoefer <henning.schiebenhoefer@posteo.de>
2017-06-26 13:52:10 +0200

    cascade_simulation-py: method stub for removal of spectra from xi_results

--- cascade_simulation.py
+++ cascade_simulation.py
@@ -8,7 +8,7 @@ FDR-conditions from all successive XiResults PSMs. This process is repeated for 
 import pandas as pd
 import timeit
 import os
-from lib.pipeline import xifdr_execution
+from lib.pipeline import XiFdrWrapper
 
 
 pd.options.display.max_rows = 999
@@ -19,9 +19,9 @@ class Experiment:
     """
     one object per MS analysis
     """
-    def __init__(self, ordered_tuple_of_xi_results, exp_name):
+    def __init__(self, ordered_list_of_xi_results, exp_name):
         self.name = exp_name
-        self.ordered_tuple_of_xi_results = ordered_tuple_of_xi_results
+        self.ordered_list_of_xi_results = ordered_list_of_xi_results
 
 
 def guess_dtypes_of_columns(
@@ -70,18 +70,44 @@ def xifdr_result_to_spectra_df(xifdr_results):
 df = pd.read_csv(filename, dtype=dtypes)
 print df.dtypes
 
+def remove_spectra_from_xi_results(df_of_spectra_to_remove, ordered_list_of_xi_results, xi_result_dir, **kwargs):
+    # read dtypes for xiresult files, either from from standard dtypes.csv, guess from standard xiresult.csv or from
+        # provides xiresult.csv
+    # for xi_result in ordered_list_of_xi_results:
+        # create subdir, named after specific dir, where xi_result is from
+        # read xi_result into pandas dataframe, respecting dtypes
+        # remove spectra specified by df_of_spectra_to_remove from xi_result_dataframe
+        # store xi_result_dataframe in csv in created subdir
+    # return list_of_processed_xi_results
+    
 
-def simulation_for_single_exp(ordered_tuple_of_xi_results, xifdr_settings_dict, out_dir):
-    # TODO work on tuples rather than list
-    while ordered_tuple_of_xi_results: # TODO macht "1" Sinn?
-        """ordered_tuple_of_xi_results has to be ordered by increasing DB size"""
-        xi_result = ordered_tuple_of_xi_results[0]
-        remaining_ordered_list_of_xi_results = ordered_tuple_of_xi_results[1:]
-        xifdr_results = xi_fdr(xi_result)
+def simulation_for_single_exp(ordered_list_of_xi_results, xifdr_settings_dict, out_dir):
+    while ordered_list_of_xi_results: # TODO macht "1" Sinn?
+        """ordered_list_of_xi_results has to be ordered by increasing DB size"""
+        xi_result = ordered_list_of_xi_results[0]
+        remaining_ordered_list_of_xi_results = ordered_list_of_xi_results[1:]
+
+        # read subdir out of filepath
+        filename = xi_result
+        for i in range(2):
+            filename = os.path.split(filename)[0]
+        subdir = os.path.split(filename)[1]
+
+        # create dirname for this specific iteration of loop
+        result_dir = os.path.join(out_dir, subdir)
+        xifdr_out_dir = os.path.join(result_dir, "xifdr_output")
+        xifdr_results = XiFdrWrapper.xifdr_execution(
+            xifdr_input_csv=xi_result,
+            xifdr_output_dir=xifdr_out_dir,
+            pepfdr=xifdr_settings_dict['xifdr_settings']['pepfdr'],
+            reportfactor=xifdr_settings_dict['xifdr_settings']['reportfactor'],
+            additional_xifdr_arguments=xifdr_settings_dict['xifdr_settings']['additional_xifdr_arguments']
+        )
         if remaining_ordered_list_of_xi_results:
             df_of_spectra_to_remove = xifdr_result_to_spectra_df(xifdr_results)
-            remaining_ordered_list_of_xi_results = remove_spectra_from_xi_results(df_of_spectra_to_remove, remaining_ordered_list_of_xi_results, out_dir)
-        ordered_tuple_of_xi_results = remaining_ordered_list_of_xi_results
+            xi_result_dir = os.path.join(result_dir, "xi_output")
+            remaining_ordered_list_of_xi_results = remove_spectra_from_xi_results(df_of_spectra_to_remove, remaining_ordered_list_of_xi_results, xi_result_dir)
+        ordered_list_of_xi_results = remaining_ordered_list_of_xi_results
 #
 
 def main_function(list_of_experiments, xifdr_settings_dict, out_dir):
@@ -89,7 +115,7 @@ def main_function(list_of_experiments, xifdr_settings_dict, out_dir):
         # set output basedir for this experiment
         exp_out_dir = os.path.join(out_dir, exp.name)
         simulation_for_single_exp(
-            ordered_tuple_of_xi_results=exp.ordered_tuple_of_xi_results,
+            ordered_list_of_xi_results=exp.ordered_tuple_of_xi_results,
             xifdr_settings_dict=xifdr_settings_dict,
             out_dir=exp_out_dir
         )
